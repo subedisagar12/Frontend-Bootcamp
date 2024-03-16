faf3478b38f5b6ddae0b2fff1a25c6858718ac9e
subedisagar12 <subedisagar12@gmail.com>
2024-03-16 22:14:55 +0545

    Media query and introdution to js

---
+++ js/script.js
@@ -0,0 +1,23 @@
+// This is a alert statement
+
+// alert("Hello World");
+
+/* 
+This
+is 
+a multi line
+comment
+*/
+
+// console.log("Hello World");
+
+// Variable
+
+// let message;
+// message = "How are you?";
+
+let a = "How are you?";
+
+const PI = 3.14;
+
+console.log(PI);
