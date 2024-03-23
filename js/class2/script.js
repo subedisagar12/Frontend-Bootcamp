ff4b5d65e19141176fce93ea7d3cad4ed2b2f59d
subedisagar12 <subedisagar12@gmail.com>
2024-03-23 22:15:13 +0545

    Variables and Type conversions

---
+++ js/class2/script.js
@@ -0,0 +1,84 @@
+// Data Types
+
+// String
+let message = "hello";
+console.log(message);
+
+// Number
+let studentAge = 3.2;
+console.log(studentAge);
+
+// Boolean (true of false)
+let isOnline = false;
+
+// null
+let age = null;
+
+age = 32;
+console.log(age);
+
+// Undefined
+let name;
+
+// Operations on data types
+
+// Number Operations
+let a = 10;
+let b = 20;
+
+// Addition, Subtraction, multiplication,division operation
+let c = b / a;
+
+console.log(c);
+
+// Modulus Operator (Remainder Operator %)
+
+let num1 = 10;
+let num2 = 3;
+
+let result = num1 % num2;
+console.log(result);
+
+// Exponential Operator (Power Operator **)
+
+// let a1 = 2;
+// let a2 = 3;
+
+// let c1 = a1 ** a2;
+
+// console.log(c1);
+
+// let myResult = 2 * (4 / 2);
+// console.log(myResult);
+
+// let test;
+// let sum = test * 5;
+// console.log(test);
+// console.log(sum);
+
+// test = temp;
+// test="Hi"
+// temp = test;
+// temp="Hi"
+// console.log(temp);
+// console.log(test);
+
+// I want test value to be assigned to temp variable
+
+// console.log(typeof test);
+
+// Comparison Operator
+
+let x = "15";
+let y = 15;
+
+// Equality Operator(==)
+
+// console.log(x == y);
+
+// Strict Equality (===)
+// console.log(x === y);
+
+// Not equals to (!=)
+
+console.log(x !== y);
---
+++ js/class3/script.js
@@ -0,0 +1,70 @@
+// let a = 45;
+// let b;
+
+// Assign value of a on b
+
+// b = a;
+
+// Assign value Ram to variable b
+// b = "ram";
+
+// Can you change the value of variable b from ram to apple? If yes, how? If no, why not?
+// b = "apple";
+
+// console.log(b);
+
+// let x = 20;
+// let y = 30;
+
+// x=x+1;
+// x+=1;
+
+// let num = ++x;
+// console.log(num);
+// console.log(x);
+// ++x;
+
+// x = x - 1;
+// x -= 1;
+
+// x--;
+// --x;
+
+// console.log(x);
+
+// I need a new variable z whose value must be the product of x and y. Achieve this in a single line;
+// let z = x * y;
+
+// console.log(z);
+
+// Decrease the value of x by 30.
+// x=x-30;
+// x -= 30;
+
+// Half the value of y
+// y = y / 2;
+// y /= 2;
+
+// String conversion
+let a = null;
+
+let b = String(a);
+
+// console.log(b);
+
+// Numeric Conversion
+let age = false;
+
+let ageNum = Number(age);
+// console.log(ageNum);
+
+// Boolean Conversion
+let t = "false";
+let bool = Boolean(t);
+// console.log(bool);
+
+// Truthy and Falsy Value
+
+let num1 = "10";
+let num2 = 10;
+console.log(num1 !== num2);
