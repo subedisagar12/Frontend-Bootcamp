6f07f86f61d6d6fbe739703f2849069df6c715df
subedisagar12 <subedisagar12@gmail.com>
2024-04-06 22:00:21 +0545

    Loop in js

---
+++ js/class4/script.js
@@ -0,0 +1,79 @@
+// Browser Interaction
+
+// Alert
+// alert("Hello");
+
+// prompt
+// let age = prompt("Enter your age: ");
+
+// console.log(age);
+
+// confirm
+
+// let confirmation = confirm("Do you want to continue?");
+
+// console.log(confirmation);
+
+// Conditional Statement or Condition Branching
+
+// if statement
+
+// if(condition){
+//     // If condition is true this part will get executed
+// }
+
+// let age = prompt("Enter your age: ");
+
+// if (age > 21) {
+//   alert("You are eligible for driving license.");
+// } else {
+//   alert("You are not eligible for driving license");
+// }
+
+// Ask user to enter a name. If user enters the name "Harry", alert him saying I recognize you, otherwise alert him saying I don't recognize you.
+
+// let Name = prompt("Enter your name.");
+// if (Name == "Harry") {
+//   alert("I recognize you.");
+// } else if (Name == "Ethan") {
+//   alert("I recognize you too.");
+// } else {
+//   alert("I don't recognize you.");
+// }
+
+// if ("0") {
+//   console.log("I will run");
+// }
+
+// Ask an user to enter his role in an office. if the role of an user is "Employee" show message "Hello". If the role of an user is "Director", show message "Greetings" else print message You don't have any role.
+
+// Logical Operators (OR, AND, NOT)
+
+// OR Operator (||)
+
+// let orResult = false || false || true || false || false;
+
+// console.log(orResult);
+
+// And Operator (&&)
+
+// let andResult = true && true && true && true;
+// console.log(andResult);
+
+// if (1 > 2 && 2 > 4) {
+//   console.log("Hello");
+// }
+
+// Ask user for their age. If the age of user is less than 18 or greater than 75, print message you cannot vote. Else print message you can vote
+
+// let age = prompt("Please enter your age.");
+
+// if (age < 18 || age > 75) {
+//   alert("you cannot vote.");
+// } else {
+//   alert("You can Vote.");
+// }
+
+let isActive = !(2 > 3);
+
+console.log(isActive);
---
+++ js/class5/script.js
@@ -0,0 +1,136 @@
+// Loops
+
+// while loop
+
+// while (2 > 3) {
+//   // code to be repeated (Loop Body)
+//   console.log("Hello");
+// }
+
+// let i = 1;
+
+// while (i < 3) {
+//   console.log("Hello");
+//   i = i + 1;
+// }
+
+// Write a while loop that prints Hello World 50 times
+
+// let i = 5;
+// while (i < 55) {
+//   console.log("Hello");
+//   i = i + 1;
+// }
+
+// let i = 1;
+
+// while (i < 51) {
+//   console.log("Hello World");
+//   i = i + 1;
+// }
+
+// let i = 0;
+
+// while (i < 50) {
+//   console.log(i);
+//   i++;
+// }
+
+//1. Control variable Initialization
+//2. Condition Checking
+//3. Code Execution
+// Loop through 2,3 until condition is false
+
+// let i = 50;
+// while (i > 0) {
+//   console.log("Hello");
+//   i--;
+// }
+
+// Do While Loop
+
+// Syntax
+
+// do {
+//   // code to be executed
+// } while (condition);
+
+//1. Control variable Initialization
+//2.Code Execution
+//3.  Condition Checking
+// Loop through 2,3 until condition is false
+
+// let i = 0;
+// do {
+//   console.log("Hello");
+//   i++;
+// } while (i < 0);
+
+// while (i < 0) {
+//   console.log("Hello");
+//   i++;
+// }
+
+// For loop
+
+// let i=0;
+// while (i < 50) {
+//   console.log(i);
+//   i++;
+// }
+
+// let i=50;
+// while (i > 0) {
+//   console.log(i);
+//   i--;
+// }
+
+// for (let i = 0; i < 50; i++) {
+//   // code to be executed
+//   console.log(i);
+// }
+
+//1. Control variable Initialization
+//2. Condition Checking
+//3. Code Execution
+// Loop through 2,3 until condition is false
+
+// Write a for loop to print a number from 250 to 50;
+// for (let i = 250; i >= 5; i--) {
+//   console.log(i);
+// }
+
+// for (let i = 250; i >= 50; i = i - 1) {
+//   console.log(i);
+// }
+
+// console.log(11 % 3);
+
+// for (let i = 0; i <= 100; i++) {
+//   if (i % 2 !== 0) {
+//     console.log(i);
+//   }
+// }
+
+// Breaking the loop
+
+// for (let i = 0; i < 500; i++) {
+//   console.log(i);
+//   if (i === 305) {
+//     break;
+//   }
+// }
+
+// Skipping the loop or continue statement
+
+// for (let i = 0; i <= 10; i++) {
+//   if (i === 7) {
+//     continue;
+//   }
+//   console.log(i);
+// }
+
+// Take a input from the user asking him the number he want to print. Then by using any of the loop studied, print the numbers from 0 to the number entered by user.
+
+let name = prompt("Your name: ");
+console.log(name);
